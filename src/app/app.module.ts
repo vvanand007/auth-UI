9a225d17b6aaeee0518b3c1be1a25706de877588
vvanand007 <coolvvanand@gmail.com>
2020-03-22 23:08:17 +0530

    register and login page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { FormsModule } from "@angular/forms";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { HttpClientModule } from "@angular/common/http";
-import { RegisterLoginComponent } from './register-login/register-login.component';
+import { RegisterLoginComponent } from "./register-login/register-login.component";
 
 @NgModule({
   declarations: [AppComponent, RegisterLoginComponent],
