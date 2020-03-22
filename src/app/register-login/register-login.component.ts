4f0e6692095df65092c69d61cf6420f14ef8b521
vvanand007 <coolvvanand@gmail.com>
2020-03-22 23:46:21 +0530

    getting credentials

--- src/app/register-login/register-login.component.ts
+++ src/app/register-login/register-login.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-register-login',
-  templateUrl: './register-login.component.html',
-  styleUrls: ['./register-login.component.css']
+  selector: "app-register-login",
+  templateUrl: "./register-login.component.html",
+  styleUrls: ["./register-login.component.css"]
 })
 export class RegisterLoginComponent implements OnInit {
+  constructor() {}
+  public emailRegister = "";
+  public nameRegister = "";
+  public passwordRegister = "";
+  public emailLogin = "";
+  public passwordLogin = "";
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  onRegister() {
+    console.log("firing register");
   }
 
+  onLogin() {
+    console.log("logined");
+  }
 }
